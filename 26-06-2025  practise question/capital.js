f2eb52a9773a41a9981f874fe696cefb444eab5d
himanshup009 <160726672+himanshup009@users.noreply.github.com>
2025-07-01 12:34:06 +0530

    Add files via upload

---
+++ 26-06-2025  practise question/capital.js
@@ -0,0 +1,6 @@
+function capitalizeWords(sentence) {
+  return sentence.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+}
+
+console.log(capitalizeWords("hello world from javascript"));
+
---
+++ 26-06-2025  practise question/flatten.js
@@ -0,0 +1,4 @@
+let arr = [1, [2, [3, 4], 5], 6];
+let flatArray = arr.flat(2);
+console.log(flatArray);
+
