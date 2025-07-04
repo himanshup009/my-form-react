b1ff0b23713bee604f67546f32e6f454043552a6
himanshup009 <160726672+himanshup009@users.noreply.github.com>
2025-07-04 11:58:05 +0530

    Delete 26-06-2025  practise question directory

--- 26-06-2025  practise question/capital.js
+++
@@ -1,6 +0,0 @@
-function capitalizeWords(sentence) {
-  return sentence.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
-}
-
-console.log(capitalizeWords("hello world from javascript"));
-
--- 26-06-2025  practise question/duplicate.js
+++
@@ -1,6 +0,0 @@
-function remove(arr) {
-  return [...new Set(arr)];
-}
-
-console.log(remove([1, 2, 2, 3, 4, 4, 5]));
-
